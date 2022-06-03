30680f5b3471fbe343614649238db1a8fd816927
hitanshu310 <hitanshu98@gmail.com>
2022-06-03 23:20:54 +0530

    removing unnecessary dependencies

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -1,19 +1,7 @@
 package model;
-
-import Strategy.GameInitStrategy;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
-import model.Board;
-import model.Player;
-
-import java.security.cert.CertPathValidatorSpi;
-import java.sql.SQLOutput;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
-import java.util.regex.Pattern;
 
 @Getter
 public class Game {
